2e101d3f6a34fa586699c346430e4879eea73612
hannanuddin78 <hannanuddin78@gmail.com>
2020-10-29 14:26:24 +0600

    firebase update

--- src/components/Login/firebase.config.js
+++ src/components/Login/firebase.config.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyCy9BAWUH4_vdqer7o620tSW_hie33CY3g",
-    authDomain: "fireme-auth.firebaseapp.com",
-    databaseURL: "https://fireme-auth.firebaseio.com",
-    projectId: "fireme-auth",
-    storageBucket: "fireme-auth.appspot.com",
-    messagingSenderId: "911025084418",
-    appId: "1:911025084418:web:4e859c525fa70fc555af1b"
-  };
-  
-    export default firebaseConfig;
+  apiKey: "AIzaSyCUEJPuF3JsyQY7B3JPCRcWaraI_MnDL6I",
+  authDomain: "ema-john-simple-my.firebaseapp.com",
+  databaseURL: "https://ema-john-simple-my.firebaseio.com",
+  projectId: "ema-john-simple-my",
+  storageBucket: "ema-john-simple-my.appspot.com",
+  messagingSenderId: "632061977046",
+  appId: "1:632061977046:web:b4eaedaf76be2efa444477",
+};
+
+export default firebaseConfig;
